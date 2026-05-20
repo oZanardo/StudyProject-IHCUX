bc6653218655b84f9f4668b7d8af5dbf61e1d61d
oZanardo <joaovzanardo@gmail.com>
2026-05-20 20:34:02 -0300

    First commit

---
+++ src/main/java/com/spring/atres/ATresApplication.java
@@ -0,0 +1,13 @@
+package com.spring.atres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ATresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ATresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/atres/ATresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.atres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ATresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
